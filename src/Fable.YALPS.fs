6a07bc4e401f1725bd9a97ef251780c898531eb2
Ivordir <Ivordir@pm.me>
2023-03-10 19:36:03 -0500

    Update to v0.4.x Update readme

--- src/Fable.YALPS.fs
+++ src/Fable.YALPS.fs
@@ -5,8 +5,7 @@ open Fable.Core
 /// Specifies the bounds for the total of a value.
 type Constraint =
   /// The total should be equal to this number.
-  /// In the case that `min` or `max` are also defined,
-  /// this is used instead.
+  /// In the case that `min` or `max` are also defined, this is used instead.
   abstract equal: float option
 
   /// The total should be greater paket than or equal to this number.
@@ -49,7 +48,7 @@ module Operators =
   let inline (===) ``constraint`` value = ``constraint``, equalTo value
 
 /// Indicates whether to `Maximize` or `Minimize` the objective.
-type [<StringEnum>] Direction =
+type [<StringEnum>] OptimizationDirection =
   | Maximize
   | Minimize
 
@@ -58,7 +57,7 @@ type [<StringEnum>] Direction =
 /// JS strict equality (===) and not structural equality.
 type Model<'VariableKey, 'ConstraintKey> =
   /// Indicates whether to `Maximize` or `Minimize` the objective.
-  abstract direction: Direction
+  abstract direction: OptimizationDirection
 
   /// <summary>
   /// The key of the value to maximize or minimize.
@@ -68,8 +67,8 @@ type Model<'VariableKey, 'ConstraintKey> =
   /// ```
   /// {| direction = Maximize
   ///    objective = "obj"
-  ///    constraints = [ "obj", lessEq 100; ... ]
-  ///    variables = [ ... ] |}
+  ///    constraints = [ "obj", lessEq 100 ]
+  ///    variables = [ (* ... *) ] |}
   /// ```
   /// </example>
   abstract objective: 'ConstraintKey
@@ -77,14 +76,17 @@ type Model<'VariableKey, 'ConstraintKey> =
   /// <summary>
   /// The constraints of the LP problem.
   /// Duplicate keys are not ignored.
-  /// Rather, the bounds of the duplicate constraints
-  /// are merged to become the most restrictive.
+  /// Rather, the bounds on the constraints are merged to become the most restrictive.
   /// </summary>
   /// <seealso cref="Constraint"/>
   /// <seealso cref="Operators"/>
   /// <example>
   /// ```
-  /// {| constraints = [ "a", lessEq 7; "b", equalTo 22; "c" &lt;== 5 ]; ... |}
+  /// let constraints = [
+  ///     "a", lessEq 7
+  ///     "b", equalTo 22
+  ///     "c" &lt;== 5
+  /// ]
   /// ```
   /// </example>
   abstract constraints: ('ConstraintKey * Constraint) seq
@@ -95,25 +97,25 @@ type Model<'VariableKey, 'ConstraintKey> =
   /// For these coefficients, the last entry is used in the case of duplicate `'ConstraintKey`s.
   /// Duplicate `'VariableKey`s, however, are not ignored.
   /// The order of variables is preserved in the solution,
-  /// but variables that end up with a value of `0` are excluded from the solution.
+  /// but variables that end up with a value of `0` are not included in the solution by default.
   /// </summary>
   /// <example>
   /// ```
-  /// {| variables =
-  ///      [ "x", [ "a", 2; "b", 11 ]
-  ///        "y", [ "a", 3; "c", 22 ] ]
-  ///    ... |}
+  /// let variables = [
+  ///     "x", [ "a", 2; "b", 11 ]
+  ///     "y", [ "a", 3; "c", 22 ]
+  /// ]
   /// ```
   /// </example>
   abstract variables: ('VariableKey * ('ConstraintKey * float) seq) seq
 
   /// A `seq` of variable keys that should be treated as integer.
-  /// `integers` can instead be a `boolean`, indicating whether all variables are integer or not.
+  /// `integers` can instead be a `bool`, indicating whether all variables are integer or not.
   abstract integers: U2<bool, 'VariableKey seq>
 
   /// An `seq` of variable keys that should be treated as binary
   /// (can only be 0 or 1 in the solution).
-  /// `binaries` can also be a `boolean`, indicating whether all variables are binary or not.
+  /// `binaries` can also be a `bool`, indicating whether all variables are binary or not.
   abstract binaries: U2<bool, 'VariableKey seq>
 
 /// The model representing a LP problem.
@@ -125,58 +127,69 @@ type Model = Model<string, string>
 module Model =
   /// Creates a model with no integer or binary variables.
   let inline create
-    (direction: Direction)
+    (direction: OptimizationDirection)
     (objective: 'ConstraintKey)
     (constraints: ('ConstraintKey * Constraint) seq)
-    (variables: ('VariableKey * #seq<'ConstraintKey * float>) seq) =
-    {| direction = direction
-       objective = objective
-       constraints = constraints
-       variables = variables |}
-    |> JsInterop.(!!) : Model<'VariableKey, 'ConstraintKey>
+    (variables: ('VariableKey * #seq<'ConstraintKey * float>) seq)
+    : Model<'VariableKey, 'ConstraintKey>
+    =
+    unbox {|
+      direction = direction
+      objective = objective
+      constraints = constraints
+      variables = variables
+    |}
 
   /// Creates model with all variables indicated as integer.
   let inline createAllInteger
-    (direction: Direction)
+    (direction: OptimizationDirection)
     (objective: 'ConstraintKey)
     (constraints: ('ConstraintKey * Constraint) seq)
-    (variables: ('VariableKey * #seq<'ConstraintKey * float>) seq) =
-    {| direction = direction
-       objective = objective
-       constraints = constraints
-       variables = variables
-       integers = true |}
-    |> JsInterop.(!!) : Model<'VariableKey, 'ConstraintKey>
+    (variables: ('VariableKey * #seq<'ConstraintKey * float>) seq)
+    : Model<'VariableKey, 'ConstraintKey>
+    =
+    unbox {|
+      direction = direction
+      objective = objective
+      constraints = constraints
+      variables = variables
+      integers = true
+    |}
 
   /// Creates a model with all variables indicated as binary.
   let inline createAllBinary
-    (direction: Direction)
+    (direction: OptimizationDirection)
     (objective: 'ConstraintKey)
     (constraints: ('ConstraintKey * Constraint) seq)
-    (variables: ('VariableKey * #seq<'ConstraintKey * float>) seq) =
-    {| direction = direction
-       objective = objective
-       constraints = constraints
-       variables = variables
-       binaries = true |}
-    |> JsInterop.(!!) : Model<'VariableKey, 'ConstraintKey>
+    (variables: ('VariableKey * #seq<'ConstraintKey * float>) seq)
+    : Model<'VariableKey, 'ConstraintKey>
+    =
+    unbox {|
+      direction = direction
+      objective = objective
+      constraints = constraints
+      variables = variables
+      binaries = true
+    |}
 
-  /// Creates a model, marking the provided `integers` and `binaries` variables
-  /// as integer and binary respectively.
+  /// Creates a model, marking the provided `integers` and `binaries` variables as integer and binary respectively.
   let inline createInteger
-    (direction: Direction)
+    (direction: OptimizationDirection)
     (objective: 'ConstraintKey)
     (constraints: ('ConstraintKey * Constraint) seq)
     (variables: ('VariableKey * #seq<'ConstraintKey * float>) seq)
     (integers: 'VariableKey seq)
-    (binaries: 'VariableKey seq) =
-    {| direction = direction
-       objective = objective
-       constraints = constraints
-       variables = variables
-       integers = integers
-       binaries = binaries |}
-    |> JsInterop.(!!) : Model<'VariableKey, 'ConstraintKey>
+    (binaries: 'VariableKey seq)
+    : Model<'VariableKey, 'ConstraintKey>
+    =
+    unbox {|
+      direction = direction
+      objective = objective
+      constraints = constraints
+      variables = variables
+      integers = integers
+      binaries = binaries
+    |}
 
 /// <summary>This indicates what type of solution, if any, the solver was able to find.</summary>
 /// <seealso cref="Solution"/>
@@ -188,9 +201,6 @@ type [<StringEnum>] SolutionStatus =
   | Cycled
 
 /// The solution returned by the solver.
-/// It includes the status of the solution,
-/// the final objective result (if any),
-/// and the variable amounts.
 type Solution<'VariableKey> =
   /// `status` indicates what type of solution, if any, the solver was able to find.
   ///
@@ -202,17 +212,16 @@ type Solution<'VariableKey> =
   /// `Unbounded` indicates a variable, or combination of variables, are not sufficiently constrained.
   /// As such, the `result` of the solution will be +-infinity.
   /// `variables` in the solution might contain a variable,
-  /// in which case it is the variable that the solver happened to finish on.
-  /// This may be the unbounded variable or one of the combination of variables that are unbounded.
+  /// in which case it is the unbounded variable that the solver happened to finish on.
   ///
   /// `Timedout` indicates that the solver exited early for an integer problem.
   /// This may happen if the solver takes too long and exceeds the `timeout` option.
-  /// Similarly, the number of branch and cut iterations may exceeed `maxIterations` as set in the options.
-  /// In both of these cases, the current sub-optimal solution/result, if any, is returned.
+  /// Similarly, the number of branch and cut iterations may exceed `maxIterations` as set in the options.
+  /// In both of these cases, the current sub-optimal solution, if any, is returned.
   /// If `result` is `NaN`, then this means no integer solutions were found before the solver timed out.
   ///
   /// `Cycled` indicates that the simplex method cycled and exited.
-  /// This case is rare, but `checkCycles` can be set to `true` in the options to check for it.
+  /// This case is rare, but `checkCycles` can be set to `true` in the options to check for cycles and stop early if one is found.
   /// Otherwise, if `maxPivots` (as set in the options) is reached by the simplex method,
   /// then it is assumed that a cycle was encountered.
   /// `result` will be `NaN` in this case.
@@ -223,69 +232,59 @@ type Solution<'VariableKey> =
   /// It may also be +-infinity in the case that `status` is `Unbounded`.
   abstract result: float
 
-  /// An array of variables and their coefficients that add up to `result`
-  /// while satisfying the constraints of the problem.
+  /// An array of variables and their coefficients that add up to `result` while satisfying the constraints of the problem.
   /// Variables with a coefficient of `0` are not included in this.
-  /// In the case that `status` is `Unbounded`,
-  /// `variables` may contain one variable which is (one of) the unbounded variable(s).
+  /// In the case that `status` is `Unbounded`, `variables` may consist of one variable which is (one of) the unbounded variable(s).
   abstract variables: ('VariableKey * float) array
 
 /// The solution returned by the solver.
-/// It includes the status of the solution,
-/// the final objective result (if any),
-/// and the variable amounts.
 type Solution = Solution<string>
 
 // Options is declared as a record to allow record-update syntax.
 
 /// The options for the solver.
 type Options = {
-  /// Numbers equal to or less than the provided precision are treated as zero.
+  /// Numbers with magnitude equal to or less than the provided precision are treated as zero.
   /// Similarly, the precision determines whether a number is sufficiently integer.
   /// The default value is `1E-8`.
   precision: float
 
   /// In rare cases, the solver can cycle.
-  /// This is usually the case when the number of pivots exceeds `maxPivots`.
-  /// Alternatively, setting this to `true` will cause
-  /// the solver to explicitly check for cycles.
-  /// (The solution will have the `Cycled` status if a cycle is detected.)
+  /// This is assumed to be the case when the number of pivots exceeds `maxPivots`.
+  /// Setting this to `true` will cause the solver to explicitly check for cycles and stop early if one is found.
+  /// Note that checking for cycles may incur a small performance overhead.
   /// The default value is `false`.
   checkCycles: bool
 
   /// This determines the maximum number of pivots allowed within the simplex method.
   /// If this is exceeded, then it assumed that the simplex method cycled,
   /// and the returned solution will have the `Cycled` status.
-  /// If your problem is *very* large, you *may* have to set this option higher.
-  /// The default value is `4096`.
+  /// If your problem is very large, you may have to set this option higher.
+  /// The default value is `8192`.
   maxPivots: int
 
   /// This setting applies to integer problems only.
-  /// If an integer solution is found within `(1 +- tolerance) *
-  /// {the problem's non-integer solution}`,
+  /// If an integer solution is found within
+  /// `(1 +- tolerance) * {the problem's non-integer solution}`,
   /// then this approximate integer solution is returned.
-  /// This is helpful for large integer problems where
-  /// the most optimal solution becomes harder to find,
-  /// but other solutions that are relatively close to
-  /// the optimal one may be much easier to find.
-  /// The default value is `0` (only find the most optimal solution).
+  /// For example, a tolereance of `0.05` allows integer solutions found within 5% of the non-integer solution to be returned.
+  /// This is helpful for large integer problems where the most optimal solution becomes harder to find,
+  /// but other solutions that are relatively close to the optimal one may be much easier to find.
+  /// The default value is `0.0` (only find the most optimal solution).
   tolerance: float
 
   /// This setting applies to integer problems only.
   /// It specifies, in milliseconds, the maximum amount of time
   /// the main branch and cut portion of the solver may take before timing out.
-  /// In the case the solver does time out,
-  /// the returned solution will have the `Timedout` status.
-  /// The current sub-optimal solution, if any, is returned as well in this case.
+  /// If a time out occurs, the returned solution will have the `Timedout` status.
+  /// Also, if any sub-optimal solution was found before the time out, then it is returned as well.
   /// The default value is infinity (no timeout).
   timeout: float
 
   /// This setting applies to integer problems only.
-  /// It determines the maximum number of iterations
-  /// for the main branch and cut algorithm.
-  /// It can be used alongside or instead of `timeout`
-  /// to prevent the algorithm from taking too long.
-  /// The default value is `4096`.
+  /// It determines the maximum number of iterations for the main branch and cut algorithm.
+  /// It can be used alongside or instead of `timeout` to prevent the algorithm from taking too long.
+  /// The default value is `32768`.
   maxIterations: int
 
   /// Controls whether variables that end up having a value of `0`
@@ -305,7 +304,7 @@ module Solver =
   /// solveWith { defaultOptions with timeout = 100.0 }
   /// ```
   /// </example>
-  let [<Import("backupDefaultOptions", "yalps")>] defaultOptions: Options = jsNative
+  let [<Import("defaultOptions", "yalps")>] defaultOptions: Options = jsNative
 
   let [<Import("solve", "yalps")>] inline private importedSolve (model: Model<'VarKey, 'ConKey>, options: Options option): Solution<'VarKey> = jsNative
 
@@ -318,7 +317,7 @@ module Solver =
   /// <summary>Runs the solver on the given model and using the given options.</summary>
   /// <example>
   /// ```
-  /// solveWith { defaultOptions with timeout = 100.0 }
+  /// model |> solveWith { defaultOptions with timeout = 100.0 }
   /// ```
   /// </example>
   /// <seealso cref="Model"/>
